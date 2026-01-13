b67cf84631a5c3d870ce73984ebfcebdddae7857
Shoaib-hussain-khan <Shoaibkhan1980610@email.com>
2026-01-13 20:45:29 +0500

    add to commit

---
+++ indix.js
@@ -0,0 +1,12 @@
+/**
+ * Example JavaScript code for indix.js
+ * This script exports a simple function.
+ */
+
+function greet(name) {
+    return `Hello, ${name}!`;
+}
+
+module.exports = {
+    greet
+};
